3f05205200379125c36012b460ea87375a0a2743
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2023-12-14 19:17:35 +0700

    My projects

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,105 @@
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    countX = 0
+    countO = 0
+    for row in board:
+        for cell in row:
+            if cell == X:
+                countX += 1
+            elif cell == O:
+                countO += 1
+    return X if countX == countO else O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    return {(i, j) for i in range(3) for j in range(3) if board[i][j] == EMPTY}
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    nextBoard = [row.copy() for row in board]
+    nextBoard[action[0]][action[1]] = player(board)
+    return nextBoard
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for i in range(3):
+        if board[i][0] != EMPTY and board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] != EMPTY and board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+    if board[0][0] != EMPTY and board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] != EMPTY and board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return winner(board) != None or len(actions(board)) == 0
+
+
+def utility(board):
+    winner1 = winner(board)
+    if winner1 == X:
+        return 1
+    elif winner1 == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    def max_value(board):
+        if terminal(board):
+            return utility(board)
+        v = float('-inf')
+        for action in actions(board):
+            v = max(v, min_value(result(board, action)))
+        return v
+
+    def min_value(board):
+        if terminal(board):
+            return utility(board)
+        v = float('inf')
+        for action in actions(board):
+            v = min(v, max_value(result(board, action)))
+        return v
+
+    max_val = float('-inf')
+    best_action = None
+    for action in actions(board):
+        v = min_value(result(board, action))
+        if v > max_val:
+            max_val = v
+            best_action = action
+    return best_action
